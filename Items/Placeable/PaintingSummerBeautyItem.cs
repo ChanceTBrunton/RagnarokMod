0f64842c3afccd13c48d6d8dc51379b857759b19
ChanceTBrunton <chancebrunton@ymail.com>
2019-05-12 02:10:21 -0400

    Painting now works for the most part.

---
+++ Items/Placeable/PaintingSummerBeautyItem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RagnarokMod.Items.Placeable
+{
+    public class PaintingSummerBeautyItem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Summer Beauty");
+            Tooltip.SetDefault("I see nothing wrong with this picture.");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 48; // hitbox width
+            item.height = 32; // hitbox height
+            item.useTime = 20; // Speed before reuse
+            item.useAnimation = 20; // Animation Speed
+            item.useStyle = 1; // broadsword
+            item.noUseGraphic = true;
+            
+            item.value = 50;
+            item.rare = 2;
+            item.UseSound = SoundID.Item1;
+            item.autoReuse = true;
+            item.consumable = true;
+            item.createTile = mod.TileType("PaintingSummerBeautyTile");
+            item.maxStack = 999;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(mod.ItemType("UraniumBarItem"), 1);
+            recipe.AddTile(TileID.Furnaces);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+    }
+}
---
+++ Tiles/PaintingSummerBeautyTile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria.ObjectData;
+using Terraria.DataStructures;
+
+namespace RagnarokMod.Tiles
+{
+    public class PaintingSummerBeautyTile : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileFrameImportant[Type] = true; // ???
+            Main.tileSolid[Type] = false;
+            Main.tileLighted[Type] = true; // allows block to emit light, see ModifyLight() method
+  
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3Wall);
+            TileObjectData.newTile.Height = 1;// 2;
+            TileObjectData.newTile.Width = 1;// 3; 
+            TileObjectData.newTile.CoordinateHeights = new int[] { 360 };//, 16, 16 };
+            TileObjectData.newTile.CoordinateWidth = 252;
+            TileObjectData.newTile.DrawYOffset = -344;
+            //TileObjectData.newTile.Origin = new Point16(0,0);
+            TileObjectData.addTile(Type);
+
+            drop = mod.ItemType("PaintingSummerBeautyItem"); // what item drops after destroying the tile
+            soundType = 21;
+            soundStyle = 1;
+            //mineResist = 4f;
+            //minPick = 200; // minimum pickaxe power needed to mine
+
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Summer Beauty Painting");
+            AddMapEntry(new Color(175, 13, 166), name);
+        }
+
+        // set the color of light emitted
+        public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
+        {
+            r = 1;
+            g = 1;
+            b = 1;
+        }
+    }
+}
